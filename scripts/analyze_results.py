52fb63a9ac854177ec550b28498b6485a3f9b884
miloszwasacz <wasacz.dev@gmail.com>
2026-04-08 14:19:07 +0200

    Add scripts for gathering and analyzing data

---
+++ scripts/analyze_results.py
@@ -0,0 +1,58 @@
+import numpy as np
+import sys
+
+def percent_to_float(x):
+    return float(x.rstrip('%'))
+
+def load(path):
+    return np.genfromtxt(path, dtype=None, delimiter=',', names=True, converters={12: percent_to_float, 24: percent_to_float})
+
+if len(sys.argv) < 3:
+    print("Usage: analyze_results <workload> <predictors...>")
+    sys.exit()
+
+workload = sys.argv[1]
+names = [sys.argv[i] for i in range(2, len(sys.argv))]
+stats = [load(f"results/{name}/{workload}_results.csv") for name in names]
+
+# TODO Group per workload
+
+for i in range(len(names)):
+    print(f"{names[i]}:")
+    data = stats[i]
+    for field in ["MR", "MPKI", "CycWPPKI"]:
+        print(f"\t{field}:")
+        field_data = np.array(data[field])
+        avg = np.mean(field_data)
+        std = np.std(field_data)
+        min_idx = np.argmin(field_data)
+        max_idx = np.argmax(field_data)
+        suffix = '%' if "MR" in field else ''
+        print(f"\t\tAvg:\t{avg:8.4f}{suffix}")
+        # if not "MR" in field:
+        #     print(f"\t\tStd:\t{std:8.4f}{suffix}")
+        print(f"\t\tMin:\t{field_data[min_idx]:8.4f}{suffix} ({data["Run"][min_idx]})")
+        print(f"\t\tMax:\t{field_data[max_idx]:8.4f}{suffix} ({data["Run"][max_idx]})")
+    print()
+
+for i in range(len(names)):
+    for j in range(i):
+        print(f"\n{names[j]} vs {names[i]}:")
+        data1 = stats[j]
+        data2 = stats[i]
+        for field in ["MPKI", "CycWPPKI"]:
+            print(f"\t{field} diff:")
+            diff = np.array(data1[field]) - np.array(data2[field])
+            diff_abs = np.absolute(diff)
+            avg = np.mean(diff)
+            std = np.std(diff)
+            min_idx = np.argmin(diff_abs)
+            max_idx = np.argmax(diff_abs)
+            suffix = '%' if "MR" in field else ''
+            print(f"\t\tAvg:\t{avg:8.4f}{suffix}")
+            # if not "MR" in field:
+            #     print(f"\t\tStd:\t{std:8.4f}{suffix}")
+            print(f"\t\tMin:\t{diff[min_idx]:8.4f}{suffix} ({data1["Run"][min_idx]})")
+            print(f"\t\tMax:\t{diff[max_idx]:8.4f}{suffix} ({data1["Run"][max_idx]})")
+        print()
+
---
+++ scripts/run_traces.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+traces=(
+	"media"
+	"compress"
+	"fp"
+	"infra"
+	"web"
+	"int"
+)
+
+if [ $# -ne 1 ]; then
+    echo "Usage: run_traces.sh <cbp-dir>"
+    exit 1
+fi
+
+CBP_DIR="${1}"
+
+for trace in ${traces[@]}; do
+    echo "===${trace}==="
+    time python3 "scripts/trace_exec_training_list.py" --trace_dir "traces/15883615/${trace}" --results_dir "results/${CBP_DIR}"
+    mv "results/${CBP_DIR}/results.csv" "results/${CBP_DIR}/${trace}_results.csv" || true
+done
+
