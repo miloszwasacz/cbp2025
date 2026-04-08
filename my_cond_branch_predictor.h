cac47a9df1204a8196a9ce2a13aa83688e4eff26
miloszwasacz <wasacz.dev@gmail.com>
2026-04-08 14:20:38 +0200

    Add Apple Firestorm implementation

--- my_cond_branch_predictor.h
+++ my_cond_branch_predictor.h
@@ -2,8 +2,10 @@
 
 #include "impls/intel/skylake.h"
 #include "impls/tage2006.h"
+#include "impls/firestorm.h"
 #include "cbp2016_tage_sc_l.h"
 
 // static CBP2016_TAGE_SC_L cond_predictor_impl;
-static tage::TAGE2006CBP cond_predictor_impl;
+// static tage::TAGE2006CBP cond_predictor_impl;
 // static intel::SkylakeCBP cond_predictor_impl;
+static firestorm::FirestormCBP cond_predictor_impl;
